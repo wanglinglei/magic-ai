0bedc4ca017168ad6621eb48accd3bcd9a68e68e
69994 <linglei.wang@bangdao-tech.com>
2025-10-16 11:28:35 +0800

    feat: 视觉风格修改

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <SplashCursor />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Header, Footer } from '@/compoents/page';
+import SplashCursor from '@/compoents/common/splashCursor/index.vue';
+</script>
+
+<style></style>
