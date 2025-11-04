ba02b4dc18f37f1839f634855100c6b741944477
69994 <linglei.wang@bangdao-tech.com>
2025-11-04 19:13:46 +0800

    feat: 添加通用样式

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'element-plus/dist/index.css';
 import App from './App.vue';
 import router from './router';
 import 'uno.css';
+import '@/assets/css/main.css';
 // 为 docx-templates 提供 Buffer polyfill
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
